b0360735753724482bfaf6bbf4b0f970a8a1c404
Ishara_Dias <isharalakshandias@gmail.com>
2024-08-30 16:54:12 +0530

    small part done

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { NAV_LINKS } from "@/constants"
 import Image from "next/image"
 import Link from "next/link"
+import Button from "./Button"
 
 const Navbar = () => {
   return (
@@ -9,6 +10,7 @@ const Navbar = () => {
       <Link href="/">
        <Image src="/hilink-logo.svg" alt="logo" width={74} height={29}/>
        </Link>  
+       
        <ul className="hidden h-full gap-12 lg:flex">
             {NAV_LINKS.map((link) => (
                 <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
@@ -16,6 +18,24 @@ const Navbar = () => {
                 </Link>
             ))}
        </ul>
+      
+      <div className="lg:flexCenter hidden">
+        <Button
+        type="button"
+        title="Login"
+        icon="/user.svg"
+        variant="btn_dark_green"
+         />
+      </div>
+
+      <Image 
+       src="menu.svg"
+       alt="menu"
+       width={32}
+       height={32}
+       className="inline-block cursor-pointer lg:hidden"     />
+
+
     </nav>
   )
 }
