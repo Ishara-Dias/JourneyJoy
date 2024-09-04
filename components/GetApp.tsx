82ac6e90350c886a844e49fa3e50f21907cebc8d
Ishara_Dias <isharalakshandias@gmail.com>
2024-09-04 10:28:24 +0530

    Done

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from './Button'
+import Image from 'next/image'
 
 const GetApp = () => {
   return (
@@ -10,22 +11,32 @@ const GetApp = () => {
           <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get for free now</h2>
           <p className="regular-16 text-gray-10">Available on ios and 
             Android</p>
-            <div>
+            <div className="flex w-full flex-col gap-3 whitespace-nowrap
+            xl:flex-row">
               <Button 
                type="button"
-               title="Download App"
+               title="App Store"
                icon="/apple.svg"
                variant="btn_white"
                full
                />
                 <Button 
                type="button"
-               title="Download App"
+               title="Play Store"
                icon="/android.svg"
                variant="btn_dark_green_outline"
                full
                />
             </div>
+        </div>
+
+        <div className="flex flex-1 items-center justify-end">
+             <Image 
+              src="/phones.svg"
+              alt="phones"
+              width={550}
+              height={870}
+              />
         </div>
    </div>
    </section>
