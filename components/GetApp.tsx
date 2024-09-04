e965c345589f1e4cddb289fd3b2ec19957e14487
Ishara_Dias <isharalakshandias@gmail.com>
2024-09-04 09:08:36 +0530

    done

--- components/GetApp.tsx
+++ components/GetApp.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
+import Button from './Button'
 
 const GetApp = () => {
   return (
    <section className="flexCenter w-full flex-col pb-[100px]">
    <div className="get-app">
-
+        <div className="z-20 flex w-full flex-1 flex-col items-start
+        justify-center gap-12">
+          <h2 className="bold-40 lg:bold-64 xl:max-w-[320px]">Get for free now</h2>
+          <p className="regular-16 text-gray-10">Available on ios and 
+            Android</p>
+            <div>
+              <Button 
+               type="button"
+               title="Download App"
+               icon="/apple.svg"
+               variant="btn_white"
+               full
+               />
+                <Button 
+               type="button"
+               title="Download App"
+               icon="/android.svg"
+               variant="btn_dark_green_outline"
+               full
+               />
+            </div>
+        </div>
    </div>
    </section>
   )
